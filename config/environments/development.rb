4b579678f52940b242ad9fa9b2c1f2ee185d650a
吉原　史朗 <>
2018-08-03 19:10:24 +0900

    configでgmail情報保護

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,14 @@ Rails.application.configure do
 
   # --------------------------------------------------------------------------
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => Settings.gmail[:user_name], #gmailアドレス
+    :password => Settings.gmail[:password], #gmailパスワード
+    :authentication => 'login',
+  }
 end
